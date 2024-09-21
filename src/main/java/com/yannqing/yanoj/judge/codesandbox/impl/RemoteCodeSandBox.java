a0912c5377b6d57c2977ad28e1502e73392abb30
yannqing <67121851@qq.com>
2024-09-21 18:39:29 +0800

    fix: fix the codesandbox can't receive the http request

--- src/main/java/com/yannqing/yanoj/judge/codesandbox/impl/RemoteCodeSandBox.java
+++ src/main/java/com/yannqing/yanoj/judge/codesandbox/impl/RemoteCodeSandBox.java
@@ -30,8 +30,7 @@ public class RemoteCodeSandBox implements CodeSandBox {
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
 
-//        String url = codeSandBoxUrl;
-        String url = "http://212.64.18.207:8091/executeCode";
+        String url = codeSandBoxUrl;
 
         String json = JSONUtil.toJsonStr(executeCodeRequest);
 
